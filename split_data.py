0a8d0312c5de9cb888edb5bd8311cfcf9f915bd7
Austin Mallie <austinmallie@sandiego.edu>
2025-03-27 23:39:14 +0000

    Finished split of agg data

--- split_data.py
+++ split_data.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Load the cleaned dataset
-    df = pd.read_csv(os.path.join(args.input_data, 'combined_agg.csv'))
+    df = pd.read_csv("/opt/ml/processing/input/combined_agg.csv")
 
     # Split: 90% train, 10% holdout
     train, holdout = train_test_split(df, test_size=0.10, random_state=42)
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     val, test = train_test_split(holdout, test_size=0.5, random_state=42)
 
     # Save outputs
-    train.to_csv(os.path.join(args.output_train, 'train.csv'), index=False)
-    val.to_csv(os.path.join(args.output_val, 'val.csv'), index=False)
-    test.to_csv(os.path.join(args.output_test, 'test.csv'), index=False)
+    train.to_csv("/opt/ml/processing/output_train/train.csv", index=False)
+    val.to_csv("/opt/ml/processing/output_val/val.csv", index=False)
+    test.to_csv("/opt/ml/processing/output_test/test.csv", index=False)
